375c2c003cd22d8ddf40afff7544ac6d09eab17b
Kolier <kolier.li@gmail.com>
2014-08-01 09:07:25 +0800

    Mod: Remove 'enabled' column in data/countries.csv. New: Utility method csvArray(). New: table*() methods. new: dataImport().

--- extension.php
+++ extension.php
@@ -12,8 +12,22 @@ use Doctrine\DBAL\Schema\Schema;
 class Extension extends \Bolt\BaseExtension
 {
 
+    public $world = array('continents', 'countries', 'languages', 'currencies');
     public $table_prefix;
     public $tables;
+    public $columns = array(
+        'countries' => array(
+            'iso2',
+            'iso3',
+            'name',
+            'longname',
+            'numcode',
+            'continent',
+        ),
+    );
+
+    // ========================================================================
+    // Extension Hook
 
     /**
      *  Extension info.
@@ -42,19 +56,26 @@ class Extension extends \Bolt\BaseExtension
     {
         // Database
         $this->table_prefix = $this->app['config']->get('general/database/prefix', "bolt_");
+        foreach ($this->world as $info) {
+            $this->tables[$info] = $this->table_prefix . $info;
+        }
         $this->tableContinents();
         $this->tableCountries();
         $this->tableLanguages();
         $this->tableCurrencies();
 
+        // Routing
+
     }
+
+    // ========================================================================
+    // Table, Data
 
     /**
      * Add continents table to database.
      */
     private function tableContinents()
     {
-        $tables['continents'] = $this->table_prefix . 'continents';
 
     }
 
@@ -63,7 +84,6 @@ class Extension extends \Bolt\BaseExtension
      */
     private function tableCountries()
     {
-        $tables['countries'] = $this->table_prefix . 'countries';
         $me = $this;
         $this->app['integritychecker']->registerExtensionTable(
             // ISO 3166-1
@@ -75,14 +95,12 @@ class Extension extends \Bolt\BaseExtension
                 $table->addColumn('iso3', 'string', array('length' => 3));
                 $table->addColumn('name', 'string', array('length' => 95));
                 $table->addColumn('longname', 'string', array('length' => 127));
-                $table->addColumn('numcode', 'smallint');
+                // @todo Find out the reason that cause issue when use 'smallint'.
+                $table->addColumn('numcode', 'integer');
                 $table->addColumn('continent', 'string', array('length' => 2));
-                $table->addColumn('continent', 'string', array('length' => 2));
-                $table->addColumn('enabled', 'boolean');
                 // Meta
                 $table->setPrimaryKey(array('id'));
                 $table->addUniqueIndex(array('iso2', 'name'));
-                $table->addIndex(array('enabled'));
                 $table->addIndex(array('continent'));
                 return $table;
             });
@@ -93,7 +111,7 @@ class Extension extends \Bolt\BaseExtension
      */
     private function tableLanguages()
     {
-        $tables['languages'] = $this->table_prefix . 'languages';
+
     }
 
     /**
@@ -101,7 +119,80 @@ class Extension extends \Bolt\BaseExtension
      */
     private function tableCurrencies()
     {
-        $tables['currencies'] = $this->table_prefix . 'currencies';
+
+    }
+
+    /**
+     *
+     */
+    private function dataImport($info)
+    {
+        // Clear old data.
+        $this->app['db']->executeUpdate('DELETE FROM ' . $this->tables[$info]);
+        // Prepare raw data.
+        $file = __DIR__ . '/data/' . $info . '.csv';
+        $data  = $this->csvArray($file);
+        // Import
+        foreach ($data as $row) {
+            switch ($info) {
+                case 'countries':
+                    // @todo Find a simpler method.
+                    $this->app['db']->executeUpdate('INSERT INTO ' . $this->tables[$info] .
+                        ' (iso2, iso3, name, longname, numcode, continent) ' .
+                        'VALUES (:iso2, :iso3, :name, :longname, :numcode, :continent)',
+                        array(
+                            ':iso2' => $row['iso2'],
+                            ':iso3' => $row['iso3'],
+                            ':name' => $row['name'],
+                            ':longname' => $row['longname'],
+                            ':numcode' => $row['numcode'],
+                            ':continent' => $row['continent'],
+                        )
+                    );
+                    break;
+            }
+        }
+    }
+
+    // ========================================================================
+    // Page
+
+    /**
+     * Data import page.
+     */
+    public  function pageImport()
+    {
+
+    }
+
+
+    // ========================================================================
+    // Utility
+
+    /**
+     * Parse .csv file into array.
+     */
+    public function csvArray($filename = '', $delimiter = ',')
+    {
+        if(!file_exists($filename) || !is_readable($filename)) {
+            return false;
+        }
+
+        $header = null;
+        $data = array();
+        if (($handle = fopen($filename, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
+                if (!$header) {
+                    $header = $row;
+                }
+                else {
+                    $data[] = array_combine($header, $row);
+                }
+            }
+            fclose($handle);
+        }
+
+        return $data;
     }
 
 }
